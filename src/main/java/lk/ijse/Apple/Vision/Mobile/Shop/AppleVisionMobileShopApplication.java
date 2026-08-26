ee23e6c50ba7f6e713db07acdf031af783fa93e2
Shaluka <shalukaranhinda@gmail.com>
2026-08-26 15:35:43 +0530

    First Commited

---
+++ src/main/java/lk/ijse/Apple/Vision/Mobile/Shop/AppleVisionMobileShopApplication.java
@@ -0,0 +1,13 @@
+package lk.ijse.Apple.Vision.Mobile.Shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppleVisionMobileShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppleVisionMobileShopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lk/ijse/Apple/Vision/Mobile/Shop/AppleVisionMobileShopApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.ijse.Apple.Vision.Mobile.Shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppleVisionMobileShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
