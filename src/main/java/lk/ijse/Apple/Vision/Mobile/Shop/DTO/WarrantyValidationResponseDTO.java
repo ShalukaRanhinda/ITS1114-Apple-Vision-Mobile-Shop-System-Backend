023f8837ccfa43f43e3eecb0ae16a0f10f3bbb84
Shaluka <shalukaranhinda@gmail.com>
2026-09-20 11:39:04 +0530

    Update WarrantyValidationResponseDTO.java

--- src/main/java/lk/ijse/Apple/Vision/Mobile/Shop/DTO/WarrantyValidationResponseDTO.java
+++ src/main/java/lk/ijse/Apple/Vision/Mobile/Shop/DTO/WarrantyValidationResponseDTO.java
@@ -1,4 +1,24 @@
 package lk.ijse.Apple.Vision.Mobile.Shop.DTO;
 
+import lk.ijse.Apple.Vision.Mobile.Shop.Enumeration.WarrantyStatus;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.time.LocalDate;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class WarrantyValidationResponseDTO {
+    private Long warrantyId;
+    private String serialNumber;
+    private boolean isValid;
+    private WarrantyStatus currentStatus;
+    private LocalDate startDate;
+    private LocalDate endDate;
+    private long remainingDays;
+    private Long orderId;
+    private String customerName;
+    private String customerPhone;
+    private String message;
 }
