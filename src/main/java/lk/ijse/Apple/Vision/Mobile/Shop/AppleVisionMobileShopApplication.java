f7b26451c6e1ba1e3d10313656955b1032d216e3
Shaluka <shalukaranhinda@gmail.com>
2026-09-20 11:39:03 +0530

    Update AppleVisionMobileShopApplication.java

--- src/main/java/lk/ijse/Apple/Vision/Mobile/Shop/AppleVisionMobileShopApplication.java
+++ src/main/java/lk/ijse/Apple/Vision/Mobile/Shop/AppleVisionMobileShopApplication.java
@@ -2,12 +2,12 @@ package lk.ijse.Apple.Vision.Mobile.Shop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class AppleVisionMobileShopApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(AppleVisionMobileShopApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(AppleVisionMobileShopApplication.class, args);
+    }
 }
---
+++ src/main/java/lk/ijse/Apple/Vision/Mobile/Shop/Scheduler/WarrantyScheduler.java
@@ -0,0 +1,4 @@
+package lk.ijse.Apple.Vision.Mobile.Shop.Scheduler;
+
+public class WarrantySchedule {
+}
