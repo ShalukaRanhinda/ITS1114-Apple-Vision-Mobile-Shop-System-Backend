00062178f474e182ee5408e2493c72be1f96816f
Shaluka <shalukaranhinda@gmail.com>
2026-09-20 11:39:04 +0530

    Update WarrantyScheduler.java

--- src/main/java/lk/ijse/Apple/Vision/Mobile/Shop/Scheduler/WarrantyScheduler.java
+++ src/main/java/lk/ijse/Apple/Vision/Mobile/Shop/Scheduler/WarrantyScheduler.java
@@ -1,4 +1,37 @@
 package lk.ijse.Apple.Vision.Mobile.Shop.Scheduler;
 
-public class WarrantySchedule {
+import jakarta.transaction.Transactional;
+import lk.ijse.Apple.Vision.Mobile.Shop.Enumeration.WarrantyStatus;
+import lk.ijse.Apple.Vision.Mobile.Shop.Repository.WarrantyRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
+
+@Component
+@Slf4j
+public class WarrantyScheduler {
+
+    private final WarrantyRepository warrantyRepository;
+
+    public WarrantyScheduler(WarrantyRepository warrantyRepository) {
+        this.warrantyRepository = warrantyRepository;
+    }
+
+
+    @Scheduled(cron = "0 0 0 * * ?")
+    @Transactional
+    public void autoExpireWarranties() {
+        log.info("Cron Triggered: Checking for outdated warranties to expire...");
+
+        LocalDate today = LocalDate.now();
+        int updatedCount = warrantyRepository.expireOutdatedWarranties(
+                WarrantyStatus.ACTIVE,
+                WarrantyStatus.EXPIRED,
+                today
+        );
+
+        log.info("Cron Completed: {} warranty record(s) marked as EXPIRED.", updatedCount);
+    }
 }
