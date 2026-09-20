68116d28d612e0225343746579db7aea205e3604
Shaluka <shalukaranhinda@gmail.com>
2026-09-20 11:39:03 +0530

    Update WarrantyClaimRequestDTO.java

--- src/main/java/lk/ijse/Apple/Vision/Mobile/Shop/DTO/WarrantyClaimRequestDTO.java
+++ src/main/java/lk/ijse/Apple/Vision/Mobile/Shop/DTO/WarrantyClaimRequestDTO.java
@@ -1,4 +1,13 @@
 package lk.ijse.Apple.Vision.Mobile.Shop.DTO;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class WarrantyClaimRequestDTO {
+    private String serialNumber;
+    private String claimReason;
 }
